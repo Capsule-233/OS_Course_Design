4f81796e8ec86801175b07573483a336ec248587
Capsule <chenjn5339@outlook.com>
2022-10-29 15:56:08 +0800

    Test

---
+++ src/com/test/Test.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author Capsule
+ * @date 2022/10/29 15:55
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
